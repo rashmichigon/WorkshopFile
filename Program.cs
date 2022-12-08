b62f2a230d1225007e00771221a6a526cfee78b7
Rashmi <rashmichigon65@gmail.com>
2022-12-08 15:06:20 +0530

    WorkshopFile

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WorkshopFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, a = 0;
+            for (int i=1;i <=n;i++)
+            {
+                if(n % i == 0)
+                {
+                    a++;
+                    if (a == 2)
+                    {
+                        Console.WriteLine("n is a prime number");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a prime number");
+                    }
+                }
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
